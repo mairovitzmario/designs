d9842d3721d44bb416aca78d5b960d2db7f58d8a
Mario <mario10rafael@gmail.com>
2026-05-15 22:10:39 +0300

    removed the isinview, only kept animatepresence

--- src/routes/starwars/-components/CloneTroopers.tsx
+++ src/routes/starwars/-components/CloneTroopers.tsx
@@ -31,75 +31,73 @@ export default function CloneTroopers() {
         className={`${styles['wrapper']} ${genericStyles['starry-sky']}`}
         style={{ '--trooper-color': trooper.color } as React.CSSProperties}
       >
-        {isInView && (
-          <div>
-            {/* Big Name */}
-            <div className={styles['name-wrapper']}>
-              <motion.h2
-                initial={{ x: 1000 }}
-                animate={{
-                  x: 0,
-                  transition: {
-                    duration: enterTextDur,
-                    delay: enterTrooperDur - 0.1,
-                    ease: 'easeOut',
-                  },
-                }}
-                exit={{ x: 1000, transition: { duration: exitAnimDur } }}
-                className={styles['name']}
-              >
-                {trooper.name}
-              </motion.h2>
-            </div>
-            {/* Description Tablet */}
-            <div className={styles['description-block-shadow']}>
-              <motion.div
-                initial={{ y: 20 }}
-                animate={{
-                  y: 0,
-                  transition: { duration: 0.5, ease: 'easeOut' },
-                }}
-                exit={{
-                  y: 20,
-                  transition: { duration: 0.25, delay: exitAnimDur },
-                }}
-                className={styles['notch']}
-              ></motion.div>
-              <article className={styles['description-block']}>
-                <div>
-                  <h2>{trooper.name}</h2>
-                  <h3>Commander: {trooper.commander}</h3>
-                  <p>{trooper.description}</p>
-                </div>
-
-                <nav className={styles['buttons']}>
-                  <button onClick={() => cycleTrooper(-1)}>
-                    <ChevronLeft />
-                  </button>
-                  <button onClick={() => cycleTrooper(1)}>
-                    <ChevronRight />
-                  </button>
-                </nav>
-              </article>
-            </div>
-            {/* Clone trooper */}
-            <motion.img
-              initial={{ y: 1000 }}
-              animate={{ y: 0, transition: { duration: enterTrooperDur } }}
-              exit={{
-                y: 1000,
+        <div>
+          {/* Big Name */}
+          <div className={styles['name-wrapper']}>
+            <motion.h2
+              initial={{ x: 1000 }}
+              animate={{
+                x: 0,
                 transition: {
-                  duration: exitAnimDur,
-                  delay: exitAnimDur,
+                  duration: enterTextDur,
+                  delay: enterTrooperDur - 0.1,
                   ease: 'easeOut',
                 },
               }}
-              className={`${styles['trooper']}`}
-              src={new URL(`../-assets/${trooper.img}`, import.meta.url).href}
-              alt={`${trooper.name} Clone Trooper`}
-            />
+              exit={{ x: 1000, transition: { duration: exitAnimDur } }}
+              className={styles['name']}
+            >
+              {trooper.name}
+            </motion.h2>
           </div>
-        )}
+          {/* Description Tablet */}
+          <div className={styles['description-block-shadow']}>
+            <motion.div
+              initial={{ y: 20 }}
+              animate={{
+                y: 0,
+                transition: { duration: 0.5, ease: 'easeOut' },
+              }}
+              exit={{
+                y: 20,
+                transition: { duration: 0.25, delay: exitAnimDur },
+              }}
+              className={styles['notch']}
+            ></motion.div>
+            <article className={styles['description-block']}>
+              <div>
+                <h2>{trooper.name}</h2>
+                <h3>Commander: {trooper.commander}</h3>
+                <p>{trooper.description}</p>
+              </div>
+
+              <nav className={styles['buttons']}>
+                <button onClick={() => cycleTrooper(-1)}>
+                  <ChevronLeft />
+                </button>
+                <button onClick={() => cycleTrooper(1)}>
+                  <ChevronRight />
+                </button>
+              </nav>
+            </article>
+          </div>
+          {/* Clone trooper */}
+          <motion.img
+            initial={{ y: 1000 }}
+            animate={{ y: 0, transition: { duration: enterTrooperDur } }}
+            exit={{
+              y: 1000,
+              transition: {
+                duration: exitAnimDur,
+                delay: exitAnimDur,
+                ease: 'easeOut',
+              },
+            }}
+            className={`${styles['trooper']}`}
+            src={new URL(`../-assets/${trooper.img}`, import.meta.url).href}
+            alt={`${trooper.name} Clone Trooper`}
+          />
+        </div>
       </section>
     </AnimatePresence>
   )
